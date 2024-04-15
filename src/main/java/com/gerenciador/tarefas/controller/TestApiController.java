d37d6ac10aa00c7593f420d18f66df98d3a44faf
Menezes <jpgam321@gmail.com>
2024-04-15 11:11:36 -0300

    Controller de Teste

--- src/main/java/com/gerenciador/tarefas/controller/TestApiController.java
+++ src/main/java/com/gerenciador/tarefas/controller/TestApiController.java
@@ -1,13 +1,20 @@
 package com.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestApiController {
 
     @GetMapping("/teste-api")
-    private String teste() {
+    public String teste() {
         return "Sucesso";
     }
+
+    @GetMapping("/teste-api-welcome")
+    public String testeWelcome(@RequestParam(name = "nome") String nome){
+        return "Olá "+ nome + ", seja bem vindo!";
+    }
+
 }
