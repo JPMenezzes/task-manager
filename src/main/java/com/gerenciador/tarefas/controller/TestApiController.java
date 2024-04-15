6e3f10dc8880ef8f220f079a1c994efa18404bbf
João Pedro Menezes <jpgam321@gmail.com>
2024-04-15 18:24:56 -0300

    Controller Test Renamed

--- src/main/java/com/gerenciador/tarefas/controller/TestApiController.java
+++ src/main/java/com/gerenciador/tarefas/controller/TestApiController.java
@@ -1,19 +1,21 @@
 package com.gerenciador.tarefas.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Controller
 public class TestApiController {
 
-    @GetMapping("/teste-api")
-    public String teste() {
+    @GetMapping("/test-api")
+    public String test() {
         return "Sucesso";
     }
 
-    @GetMapping("/teste-api-welcome")
-    public String testeWelcome(@RequestParam(name = "nome") String nome){
+    @GetMapping("/test-api-welcome")
+    public String testWelcome(@RequestParam(name = "nome") String nome){
         return "Olá "+ nome + ", seja bem vindo!";
     }
 
